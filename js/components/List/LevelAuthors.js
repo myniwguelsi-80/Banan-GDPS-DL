48e343404a0991246fcaeecd94c7c0560009fc00
myniwguelsi-80 <cumalinintableti@gmail.com>
2025-11-15 22:24:27 +0300

    Bisey yaptim yes beeee

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -22,29 +22,29 @@ export default {
                 </p>
             </template>
             <template v-else-if="creators.length === 0">
-                <div class="type-title-sm">Creator</div>
+                <div class="type-title-sm">Yapımcı</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
-                <div class="type-title-sm">Verifier</div>
+                <div class="type-title-sm">Verıfıer</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
             </template>
             <template v-else>
-                <div class="type-title-sm">Creators</div>
+                <div class="type-title-sm">Yapımcılar</div>
                 <p class="type-body">
                     <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
                         <span >{{ creator }}</span
                         ><span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
-                <div class="type-title-sm">Verifier</div>
+                <div class="type-title-sm">Verıfıer</div>
                 <p class="type-body">
                     <span>{{ verifier }}</span>
                 </p>
             </template>
-            <div class="type-title-sm">Publisher</div>
+            <div class="type-title-sm">Yayınlayan</div>
             <p class="type-body">
                 <span>{{ author }}</span>
             </p>
